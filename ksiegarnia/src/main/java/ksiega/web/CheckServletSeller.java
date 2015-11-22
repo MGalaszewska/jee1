de68510b32eca45244ed6d2b7bad6d4b82aa2f9a
MGalaszewska <marti18.1993@gmail.com>
2015-11-22 23:07:33 +0100

    Update CheckServletSeller.java

--- ksiegarnia/src/main/java/ksiega/web/CheckServletSeller.java
+++ ksiegarnia/src/main/java/ksiega/web/CheckServletSeller.java
@@ -18,34 +18,6 @@ public class CheckServletSeller extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		
-		/*
-		Book book = new Book(request.getParameter("title"),
-				request.getParameter("author"), request.getParameter("type"),
-				request.getParameter("genre"));
-
-		response.setContentType("text/html");
-		String[] requestGenres = request.getParameterValues("genre");
-		
-		String selectedGenres = "";
-		if (requestGenres == null) {
-			selectedGenres = "brak";
-		} else {
-			for (String genre : requestGenres) {
-				selectedGenres += genre + ", ";
-			}
-			selectedGenres = selectedGenres.substring(0,
-					selectedGenres.length() - 2);
-		}
-		
-		book.setGenre(selectedGenres);
-		request.setAttribute("genre", selectedGenres);
-		request.setAttribute("book", book);
-		
-		request.getRequestDispatcher("addedBook.jsp")
-				.forward(request, response);
-
-	}
-	*/
 		try {
 			Seller seller= new Seller(request.getParameter("firstName"),
 					request.getParameter("lastName"),
